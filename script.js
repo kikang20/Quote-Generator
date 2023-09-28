ad840c79f116ccd30f063b3154da8da4fae25ad0
ki kang <kikang20@yahoo.com>
2023-09-28 16:51:58 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let apiQuotes = [];
+
+// show new quote
+function newQuote () {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+async function getQuotes () {
+    const apiUrl = `https://type.fit/api/quotes`;
+    try {
+        const response = await fetch (apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+
+    }
+}
+
+//on Load 
+getQuotes();
