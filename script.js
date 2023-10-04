dd9afed0a50ebd36bad5fb720c4100b5101e55fb
ki kang <kikang20@yahoo.com>
2023-10-04 15:38:20 -0700

    final

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+
 let apiQuotes = [];
 
-// show new quote
-function newQuote () {
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+function loading () {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
 }
 
+function complete () {
+    quoteContainer.hidden = false;
+    loader.hidden= true;
+
+}
+
+
+function newQuote () {
+    loading ();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+        quoteText.textContent = quote.text;
+    } 
+    // if (quote.text.length > 50) {
+    //     quoteText.classList.add ("long-quote");
+    // } else {
+    //     quoteText.classList.remove ("long-quote");
+    // }
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+//getting quote from api
 async function getQuotes () {
+    loading ();
     const apiUrl = `https://type.fit/api/quotes`;
     try {
         const response = await fetch (apiUrl);
@@ -16,6 +51,15 @@ async function getQuotes () {
 
     }
 }
+
+//tweet quote
+function tweetQuote () {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, `_blank`);
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 //on Load 
 getQuotes();
